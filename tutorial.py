78661c27bc067f8618a68537f6655fa189794cf1
K Lavanya Reddy <117257210+kambhamlavanya12@users.noreply.github.com>
2022-12-30 15:42:45 +0530

    Add files via upload

---
+++ tutorial.py
@@ -0,0 +1,18 @@
+x=["apple", "banana", "cherry"]
+print(type(x))
+y=[]
+print(type(y))
+#This is a comment
+x = 5
+y = 10
+z = x + y
+print(z)
+x =y=z="orange"
+print(x)
+print(y)
+print(z)
+x = "Hello World"
+print(type(x))
+x = ("apple", "banana", "cherry")
+print(type(x))
+
