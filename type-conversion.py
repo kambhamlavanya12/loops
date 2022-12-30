7f67652c5f7170a3388422ba57d73564338781a9
K Lavanya Reddy <117257210+kambhamlavanya12@users.noreply.github.com>
2022-12-30 15:45:51 +0530

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,45 @@
+# a=4
+# print(type(a))
+# print(float(a))
+# print(str(a))
+
+# b=6.8
+# print(type(b))
+# print(int(b))
+# print(str(b))
+
+# a=3
+# print("The value of a is",a)
+# print("The value of a is ",a)
+
+# b=""
+# print(b,type(b))
+# c="   "
+# print(c.isspace())
+# d=7.8
+# print(type(d))
+# print(int(d))
+# print(float(d))
+# print(str(d))
+# e='67.45'
+# print(e,type(e))
+# g=float(e)
+# print(g)
+# print(int(e))
+# print(int(g))
+# a=5;b=4.5;c='py';d='5.5';e='6';f='on';g=8
+# print(a+b) #int+float=9.5
+# print(a+g) #int+int=13
+# print(b+c) #typeError
+# print(c+d) #str+str=py5.5
+# print(d+e) #str+str=5.56
+# print(e+d) #str+str=65.5
+# print(e+f) #str+str=6on
+# print(a+d) #typeError
+# print(str(a)+d) #55.5
+# print(a+int(d)) #valueError
+# print(int(d)) #valueError
+# print(int(e)) #6
+# print(float(e)) #6.0
+# print(str(g)) #8
+
